c19bc9e8f5df030890308516a1cb4fd447c8b835
Jim Chen <nchen@mozilla.com>
2015-12-17 00:19:45 -0500

    Turn differ into a class and add ability to set handlers.

--- diff.py
+++ diff.py
@@ -16,45 +16,72 @@ class Diff(object):
         # content added.
         return '+%d %s' % (self._bsize - self._asize, self._name)
 
-def diff_zip(a, b, prefix=''):
-
-    def _diff_file(name, asize, bsize):
-        ext = name.rpartition('.')[-1]
-
-        if ext in {'zip', 'apk', 'jar', 'ja'}:
-            with ZipFile(BytesIO(a.read(name))) as azip:
-                with ZipFile(BytesIO(b.read(name))) as bzip:
-                    for diff in diff_zip(azip, bzip, prefix=(name + '/')):
+class Differ(object):
+    def __init__(self):
+        def _zip_handler(name, a, b):
+            with ZipFile(a) as azip:
+                with ZipFile(b) as bzip:
+                    for diff in self._diff_zip(azip, bzip, name + '/'):
                         yield diff
-            return
 
-        if asize != bsize:
-            yield Diff(prefix + name, asize, bsize)
+        self._handlers = {
+            'zip': _zip_handler,
+            'apk': _zip_handler,
+            'jar': _zip_handler,
+            'ja':  _zip_handler,
+        }
 
-    afiles = {info.filename: info for info in a.infolist()}
+    def set_handler(self, ext, handler):
+        self._handlers[ext] = handler
 
-    for bfile in b.infolist():
-        name = bfile.filename
-        afile = afiles.pop(name, None)
+    def get_handler(self, ext):
+        return self._handlers.get(ext)
 
-        if afile:
-            # file updated.
-            for diff in _diff_file(name, afile.file_size, bfile.file_size):
-                yield diff
-            continue
+    def diff_zip(self, a, b):
+        for diff in self._diff_zip(a, b, ''):
+            yield diff
 
-        # file added.
-        yield Diff(prefix + name, 0, bfile.file_size)
+    def _diff_zip(self, a, b, prefix):
 
-    for afile in afiles.values():
-        # file deleted.
-        yield Diff(prefix + afile.filename, afile.file_size, 0)
+        def _diff_file(name, asize, bsize):
+            ext = name.rpartition('.')[-1]
+            handler = self._handlers.get(ext)
+
+            if handler:
+                for diff in handler(prefix + name,
+                                    BytesIO(a.read(name)),
+                                    BytesIO(b.read(name))):
+                    yield diff
+                return
+
+            if asize != bsize:
+                yield Diff(prefix + name, asize, bsize)
+
+        afiles = {info.filename: info for info in a.infolist()}
+
+        for bfile in b.infolist():
+            name = bfile.filename
+            afile = afiles.pop(name, None)
+
+            if afile:
+                # file updated.
+                for diff in _diff_file(name, afile.file_size, bfile.file_size):
+                    yield diff
+                continue
+
+            # file added.
+            yield Diff(prefix + name, 0, bfile.file_size)
+
+        for afile in afiles.values():
+            # file deleted.
+            yield Diff(prefix + afile.filename, afile.file_size, 0)
 
 if __name__ == '__main__':
     import sys
 
     with ZipFile(sys.argv[1]) as azip:
         with ZipFile(sys.argv[2]) as bzip:
-            for diff in diff_zip(azip, bzip):
+            differ = Differ()
+            for diff in differ.diff_zip(azip, bzip):
                 print(diff)
 
